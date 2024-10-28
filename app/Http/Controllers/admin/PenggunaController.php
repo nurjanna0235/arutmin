f0f615a52ea77e5c34c79ee0bdee7177b4127146
nurjanna0235 <nurjanna@mhs.politala.ac.id>
2024-10-28 15:58:07 +0800

    bore

--- app/Http/Controllers/admin/PenggunaController.php
+++ app/Http/Controllers/admin/PenggunaController.php
@@ -7,5 +7,10 @@ use Illuminate\Http\Request;
 
 class PenggunaController extends Controller
 {
-    
+    public function index()
+    {
+       // $dokumenpit_clearing = pit_clearing::all();
+       
+        return view('admin/pengguna/index');
+    }
 }
